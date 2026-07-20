bd39cea18e5963c74655ee95bf4e1016a4dd6baf
Karim Ahmed <kareem0101895@gmail.com>
2026-07-20 16:17:36 +0300

    Fix footer links

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,10 @@ return(
 <footer>
     <p> Copyright &copy; 2025 Madrid Store | All Rights Reserved </p>
     <div id="time"><Time/></div>
-    <a href="" target="_blank"> <FaWhatsapp/> </a>
-    <a href="" target="_blank"> <FaFacebook/> </a>
-    <a href="" target="_blank"> <FaTiktok/>  </a>
-    <a href="" target="_blank"> <FaInstagram/> </a>
+    <a href="#!" target="_blank"> <FaWhatsapp/> </a>
+    <a href="#!" target="_blank"> <FaFacebook/> </a>
+    <a href="#!" target="_blank"> <FaTiktok/>  </a>
+    <a href="#!" target="_blank"> <FaInstagram/> </a>
   </footer>
 )
 }
